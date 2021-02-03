173c41ba0333cddee335cdfb8b2be196e494900f
HARSHITHV25 <harshithv25@gmail.com>
2021-02-03 23:22:20 +0530

    Initial commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Footer = ({ data }) => {
+  if (data) {
+    var networks = data.social.map(function (network) {
+      return (
+        <li key={network.name}>
+          <a href={network.url}>
+            <i className={network.className}></i>
+          </a>
+        </li>
+      );
+    });
+  }
+
+  return (
+    <footer>
+      <div className="row">
+        <div className="twelve columns">
+          <ul className="social-links">{networks}</ul>
+
+          <ul className="copyright">
+            <li>
+              Made, Packaged and Inspected by{" "}
+              <a title="Sudo" href="https://www.sudo.ws/">
+                Sudo
+              </a>
+            </li>
+            <li>
+              <b>sudo make me a sanvich</b>
+            </li>
+          </ul>
+        </div>
+        <div id="go-top">
+          <a className="smoothscroll" title="Back to Top" href="#home">
+            <i className="icon-up-open"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
